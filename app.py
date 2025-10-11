cda7e0968eb462c2448da97f7b000f3250ce8988
Mohammad Tavakoli <tvkoli.mhmd@gmail.com>
2025-10-11 17:16:16 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 app = Flask(__name__)
-API_KEY = "c55977c391cd4736af7961fb4e863a84"
+API_KEY = "790d47bbf67d42688eb4542cb2e83949"
 @app.route("/")
 def home():
     return render_template("index.html")
